81c29c9a836c84daa0a6cda6c864931e769d712f
GabrielAdward <adga21bv@student.ju.se>
2024-09-16 01:46:17 +0200

    upda

--- BachelorThesis/frontend/src/App.svelte
+++ BachelorThesis/frontend/src/App.svelte
@@ -1,15 +1,16 @@
 <script>
-	import Router from 'svelte-spa-router'; // Import the router component
-	import { routes } from './routes.js'; // Import the routes configuration
+
 	import Navbar from './components/Navbar.svelte';
 	import Footer from './components/Footer.svelte';
+	import Home from './routes/Home.svelte';
   </script>
   
-  <Navbar/>
-<main class="min-h-screen bg-gray-100 bg-blue-950">
-	<Router {routes} useHash={false} />
-</main>
-<Footer/>
+  <Navbar />
+  <main class="min-h-screen bg-blue-950">
+	<!-- Only apply bg-blue-950 to the parent container -->
+	<Home  />
+  </main>
+  <Footer />
   
 
   
--- BachelorThesis/frontend/src/routes/Home.svelte
+++ BachelorThesis/frontend/src/routes/Home.svelte
@@ -1,3 +1,33 @@
-<div>
+<div class="main text-center pt-16">
+    <h1 class="text-6xl font-bold uppercase animate-textclip">
+        Engage Audiences with Stunning Videos
+    </h1>
+
+    <h3 class="text-3xl font-bold mt-8">
+        Boost Your Brand with High-Impact Videos
+    </h3>
+
     
 </div>
+  
+
+  
+<style>
+    /* Custom gradient and text animation */
+    h1, h3 {
+        background: linear-gradient(to right, #33B7FF 10%, #BD23FF 50%, #FCFF33 60%);
+        background-clip: text;
+        -webkit-background-clip: text;
+        color: transparent;
+        -webkit-text-fill-color: transparent;
+        background-size: 200% auto;
+        animation: textclip 3s linear infinite;
+    }
+
+    @keyframes textclip {
+        to {
+            background-position: 200% center;
+        }
+    }
+</style>
+  
